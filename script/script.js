c47ae0a4922935100ed21ba5f0d7f0ec332a1856
santosh jogdand <santoshjogdand256@gmail.com>
2024-02-18 18:17:36 +0530

    Added font and Icons for humidity and wind speed

--- script/script.js
+++ script/script.js
@@ -2,35 +2,81 @@ const API_KEY = "b9d26520f7b8a11536f284acef2aa108";
 let CityName;
 let temperature;
 let weatherText;
+let iconid;
+let humidity;
+let windspeed;
+let country;
+let feel;
 async function getWeather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
   let value = await fetch(url);
   let data = await value.json();
+  console.log(data)
   console.log("name: " + data.name);
   let notf = document.querySelector(".notf");
   let info = document.querySelector(".info");
+  info.style.display = "none";
   if (data.name == undefined) {
     notf.style.display = "flex";
     info.style.display = "none";
-    notf.style.transitionDuration = "5s";
-    info.style.transitionDuration = "5s";
     console.log("not found");
   } else {
     CityName = data.name;
+    country = data.sys.country;
+    let load = document.querySelector(".load");
     weatherText = data.weather[0].description;
     temperature = data.main.temp;
-    info.style.display = "none";
-    info.style.display = "block";
-    info.style.transitionDuration = "5s";
-    notf.style.transitionDuration = "5s";
-    console.log("Tempreture: " + data.main.temp);
-    console.log("Humidity: " + data.main.humidity);
-    console.log("Weather Description: " + data.weather[0].description);
-    console.log("Wind Speed: " + data.wind.speed);
+    iconid = data.weather[0].icon;
+    humidity = data.main.humidity
+    feel = data.main.feels_like;
+    windspeed = data.wind.speed
+    notf.style.display = "none";
+    load.style.display = "flex";
+    setTimeout(function() {
+      info.style.display = "flex";
+      load.style.display = "none";
+
+    }, 3000);
     const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    icimg = document.querySelector(".info");
-    icimg.innerHTML = `<img class = "icon" src = "${icon}">`;
-    document.querySelector(".icon").style.width = "100px";
+    let wimg = document.querySelector(".wimg");
+    let wdescription = document.querySelector(".wdescription")
+    let temper = document.querySelector(".temper");
+    let name = document.querySelector(".name");
+    let humid = document.querySelector(".humid");
+    let wind = document.querySelector(".wind");
+    if(iconid == "01d"){
+      wimg.src = "./images/01d.png"
+    }
+    else if(iconid == "01n"){
+      wimg.src = "./images/01n.png"
+    }
+    else if(iconid == "02d"){
+      wimg.src = "./images/02d.png"
+    }
+    else if(iconid == "03d" || iconid == "03n"){
+      wimg.src = "./images/03.png"
+    }
+    else if(iconid == "04d" || iconid == "04n"){
+      wimg.src = "./images/04.png"
+    }
+    else if(iconid == "09d" || iconid == "09n"){
+      wimg.src = "./images/09.png"
+    }
+    else if(iconid == "10d"){
+      wimg.src = "./images/10d.png"
+    }
+    else if(iconid == "10n"){
+      wimg.src = "./images/10n.png"
+    }
+    else if(iconid == "50d" || iconid == "50n"){
+      wimg.src = "./images/50.png"
+    }
+    weatherText = weatherText.charAt(0).toUpperCase()+(weatherText.slice(1));
+    wdescription.innerHTML = `Feels like ${feel}°C.<br>${weatherText}.`;
+    temper.innerHTML = Math.round(temperature) + "°C";
+    name.innerHTML = CityName + ", " + country;
+    humid.innerHTML = "Humidity <br>" + humidity +"%";
+    wind.innerHTML = "Wind Speed <br>" + windspeed + "m/s";
   }
 }
 
@@ -57,6 +103,7 @@ searchbtn = document.querySelector(".search");
 searchbtn.addEventListener("click", () => {
   city = document.querySelector("#city").value;
   getWeather(city);
+  
   console.log("clicked on button!!");
   console.log(city);
 });
